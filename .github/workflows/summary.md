53c09d488bdd1704d56f4e7291217369113f523d
Alexander Ribeiro <alexander2005rj@gmail.com>
2023-07-24 18:19:14 -0300

    github-actions-job-summary

---
+++ .github/workflows/summary.md
@@ -0,0 +1,5 @@
+### Cypress Results
+
+| Result |Passed :white_check_mark: | Failed :x:|Pending :hand:|Skipped:leftwards_arrow_with_hook:|Duration :clock: |
+|--|--|--|--|--|--|
+|Passing :white_check_mark:  | ${{passed}}|${{failed}}|${{pendind}}|${{skipped}}| ${{duration}} s|
--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const cypressTestResults = require("cypress-test-results");
 
 module.exports = defineConfig({
   viewportHeight: 660,
@@ -19,6 +20,20 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on, config);
+      cypressTestResults(on, config)
+      // on('before:run', details => {
+      //   console.log(JSON.stringify(details.browser.name))
+      // })
+
+      // on('after:run', results => {
+      //   console.log(JSON.stringify("Status da execução ==> " + results.status));
+      //   console.log(JSON.stringify("Nome do Browser ==> " + results.browserName));
+      //   console.log(JSON.stringify("Passed ==> " + results.totalPassed));
+      //   console.log(JSON.stringify("Failed ==> " + results.totalFailed));
+      //   console.log(JSON.stringify("Pending ==> " + results.totalPending));
+      //   console.log(JSON.stringify("Skipped ==> " + results.totalSkipped));
+      //   console.log(JSON.stringify("Duration ==> " + results.totalDuration + "s"));
+      // })
     },
   },
 })
